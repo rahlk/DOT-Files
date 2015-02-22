71814e7f86e8e0cd01bd1061196e34574c9b3b8f
Rahul Krishna <i.m.ralk@gmail.com>
2015-02-22 14:55:43 -0500

    Setup files and a backup copy of my .vimrc file

---
+++ prepVim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# Install Python Stuff..
+sudo apt-get install pip
+cd ~/.vim/bundle
+
+# Install powerline
+git clone git://github.com/Lokaltog/vim-powerline.git
+
+# Install ctrlp
+git clone https://github.com/kien/ctrlp.vim.git
+
+# Install JEDi-VIM
+git clone git://github.com/davidhalter/jedi-vim.git
+
+# Install Synastic
+git clone https://github.com/scrooloose/syntastic.git
